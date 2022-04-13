9129e62bc60caf84491072ec79d3a49faee4166f
techpassel <techpasselorg@gmail.com>
2022-04-13 20:46:33 +0530

    Completed navbar, footer and sidebar design

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
+  @ViewChild('first', { static: false })
+  firstDropdown!: ElementRef;
+  @ViewChild('second', { static: false })
+  secondDropdown!: ElementRef;
+  @ViewChild('third', { static: false })
+  thirdDropdown!: ElementRef;
+  @ViewChild('sidebar', { static: false })
+  sidebarElement!: ElementRef;
+
+  toggleShowMenu = (val: string, currentElement: HTMLElement) => {
+    let component = null;
+    switch (val) {
+      case 'first':
+        component = this.firstDropdown
+        break;
+      case 'second':
+        component = this.secondDropdown
+        break;
+      case 'third':
+        component = this.thirdDropdown
+        break;
+      default:
+        break;
+    }
+    if (component) {
+      component.nativeElement.classList.toggle('show_menu');
+      console.log(component?.nativeElement.classList);
+    }
+    currentElement.classList.toggle('transform_arrow');
+  }
+
+  toggleSidebar = (elem :HTMLElement) => {
+    this.sidebarElement.nativeElement.classList.toggle('close');
+    elem.classList.toggle('transform_arrow');
+  }
 }
