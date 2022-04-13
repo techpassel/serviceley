9afba42259e4d73ec1900faa3cfe4a05015d76e1
techpassel <techpasselorg@gmail.com>
2022-04-13 15:01:13 +0530

    Completed structure for header and footer

--- server/src/main/java/com/tp/serviceley/server/service/admin/ServiceService.java
+++ server/src/main/java/com/tp/serviceley/server/service/admin/ServiceService.java
@@ -18,7 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
